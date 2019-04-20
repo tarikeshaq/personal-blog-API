e8bbb95c08a5da3cccdd9bbabd9231bce89a0d5b
Tarik <tarikishaq@hotmail.com>
2019-04-20 09:15:43 -0700

    setup mongo connection to localhost and handlers

--- app.go
+++ app.go
@@ -1,34 +1,112 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/tarikeshaq/personal-blog-api/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func GetAllBlogsHandler(writer http.ResponseWriter, request *http.Request) {
-	writer.Write([]byte("Not yet Implemented, gets all posts"))
+var client *mongo.Client
+
+type Post = models.Post
+
+func GetAllBlogsHandler(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+	var posts []Post
+	database := client.Database("personal_api")
+	collection := database.Collection("posts")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var post Post
+		cursor.Decode(&post)
+		posts = append(posts, post)
+	}
+	if err := cursor.Err(); err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(posts)
 }
 
-func GetOneBlogHandler(writer http.ResponseWriter, request *http.Request) {
+func GetOneBlogHandler(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+
+	params := mux.Vars(request)
+	blogId, _ := primitive.ObjectIDFromHex(params["blogId"])
+	var post Post
+	collection := client.Database("personal_api").Collection("posts")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	filter := bson.D{{"_id", blogId}}
+	err := collection.FindOne(ctx, filter).Decode(&post)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + params["blogId"] + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(post)
+}
+
+func AddNewBlogHandler(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+
+	var post Post
+	_ = json.NewDecoder(request.Body).Decode(&post)
+	database := client.Database("personal_api")
+	collection := database.Collection("posts")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	result, err := collection.InsertOne(ctx, post)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(result)
+}
+
+func RemoveBlogHandler(response http.ResponseWriter, request *http.Request) {
+	response.Header().Set("content-type", "application/json")
+
 	vars := mux.Vars(request)
-	blogId := vars["blogId"]
-	writer.Write([]byte("Not yet implemented, gets one post with Id" + blogId))
-}
-
-func AddNewBlogHandler(writer http.ResponseWriter, request *http.Request) {
-	writer.Write([]byte("Not yet implemented, creates a new post using the payload from the request"))
-}
-
-func RemoveBlogHandler(writer http.ResponseWriter, request *http.Request) {
-	vars := mux.Vars(request)
-	blogId := vars["blogId"]
-	writer.Write([]byte("Not yet implemented, removes a post with ID " + blogId))
+	blogId, _ := primitive.ObjectIDFromHex(vars["blogId"])
+	database := client.Database("personal_api")
+	collection := database.Collection("posts")
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	filter := bson.D{{"_id", blogId}}
+	result, err := collection.DeleteOne(ctx, filter)
+	if err != nil {
+		response.WriteHeader(http.StatusInternalServerError)
+		response.Write([]byte(`{ "message": "` + err.Error() + `" }`))
+		return
+	}
+	json.NewEncoder(response).Encode(result)
 }
 
 func main() {
+	// setup mongodb connection
+	client, _ = mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err := client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
 	router := mux.NewRouter()
-
 	router.HandleFunc("/blogs", GetAllBlogsHandler).Methods("GET")
 	router.HandleFunc("/blogs/{blogId}", GetOneBlogHandler).Methods("GET")
 	router.HandleFunc("/blogs", AddNewBlogHandler).Methods("POST")
---
+++ models/Post.go
@@ -0,0 +1,19 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type Post struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Title     string             `json:"title,omitempty" bson:"title,omitempty"`
+	Post_type string             `json:"post_type,omitempty" bson:"post_type,omitempty"`
+	Content   Content            `json:"content,omitempty" bson:"content,omitempty"`
+	Date      string             `json:"date,omitempty" bson:"date,omitempty"`
+}
+
+type Content struct {
+	Overall     string `json:"overall,omitempty" bson:"overall,omitempty"`
+	Like        string `json:"like,omitempty" bson:"like,omitempty"`
+	Not_like    string `json:"not_like,omitempty" bson:"not_like,omitempty"`
+	Rating      int    `json:"rating,omitempty" bson:"rating,omitempty"`
+	Rating_desc string `json:"rating_desc,omitempty" bson:"rating_desc,omitempty"`
+}
